afde34a8a8a23b5dcc23826c72061716e44a8ad9
JARDEL MACIEL  DOS SANTOS <jardel.maciel22@gmail.com>
2025-02-04 10:25:11 -0300

    aula-3-anatomia-das-classes

--- aula-1/java-anatomia/src/MinhaClasse.java
+++ aula-1/java-anatomia/src/MinhaClasse.java
@@ -1,7 +1,18 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Olá, Mundo.");
+
+        String primeiroNome = "Jardel";
+        String segundoNome = "Maciel";
+        
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+    } 
     
 }
